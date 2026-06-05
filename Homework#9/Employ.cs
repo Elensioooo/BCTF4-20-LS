cdc3f4765947c67f7d2524b3564ccf9244cea1a2
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2026-06-05 15:36:43 +0700

    Add Homework#9

---
+++ Homework#9/Employ.cs
@@ -0,0 +1,148 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Reflection;
+
+namespace Homework_9
+{
+    internal class Employ
+    {
+        string _name;
+        string _surname;
+        DateTime _dateOfBirth;
+        Country _country;
+        Gender _gender;
+        Contacts _contacts;
+
+        public Employ(string name, string surname, DateTime dateOfBirth, Country country, Gender gender, Contacts contacts)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.DateOfBirth = dateOfBirth;
+            this.Country = country;
+            this.Gender = gender;
+            this.Contacts = contacts;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Name cannot be empty");
+                _name = value.Trim();
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Surname cannot be empty");
+                _surname = value.Trim();
+            }
+        }
+
+        public DateTime DateOfBirth
+        {
+            get
+            {
+                return _dateOfBirth;
+            }
+            set
+            {
+                if (value > DateTime.Now)
+                    throw new ArgumentException("Date of birth cannot be in the future");
+                _dateOfBirth = value;
+            }
+        }
+
+        public Country Country
+        {
+            get
+            {
+                return _country;
+            }
+            set
+            {
+                if (!Country.TryParse(value.ToString(), out _country))
+                    throw new ArgumentException("Invalid country");
+
+                _country = value;
+            }
+        }
+
+        public Gender Gender
+        {
+            get
+            {
+                return _gender;
+            }
+            set
+            {
+                if (!Gender.TryParse(value.ToString(), out _gender))
+                    throw new ArgumentException("Invalid gender");
+                _gender = value;
+            }
+        }
+
+        public Contacts Contacts
+        {
+            get
+            {
+                return _contacts;
+            }
+            set
+            {
+                if (!Contacts.TryParse(value.ToString(), out _contacts))
+                    throw new ArgumentException("Invalid contacts");
+                _contacts = Contacts;
+            }
+        }
+   
+    
+        public int GetAge()
+        {
+            int age = DateTime.Now.Year - DateOfBirth.Year;
+            return age;
+        }
+
+       
+        public override string? ToString()
+        {
+            return $"Name - {Name}, Surname - {Surname}, DateOfBirth - {DateOfBirth}, " +
+                $"Country - {Country}, Gender - {Gender}, Contacts - {Contacts}";
+        }
+
+    }
+
+
+    public enum Country 
+    {
+        Georgia,
+        Spain,
+        Italy,
+        Sweden
+    }
+    public enum Gender: byte
+    {
+        female,
+        male,
+        other
+    }
+
+    public enum Contacts :byte
+    {
+        email,
+        phoneNumber,
+        fax
+    }
+
+}
---
+++ Homework#9/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Homework_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employ employ1 = new Employ("Elene", "Morgoshia", new DateTime(2004, 7, 20), Country.Georgia, Gender.female, Contacts.email);
+            Employ employ2 = new Employ("Elensio", "Morgoshiaaa", new DateTime(2004, 7, 20), Country.Georgia, Gender.female, Contacts.email);
+            Employ employ3 = new Employ("Niko", "Morgoshia", new DateTime(2004, 7, 20), Country.Italy, Gender.male, Contacts.email);
+            Employ employ4 = new Employ("Emily", "something", new DateTime(2004, 7, 20), Country.Italy, Gender.female, Contacts.email);
+            Employ employ5 = new Employ("Sandra", "something", new DateTime(2004, 7, 20), Country.Spain, Gender.female, Contacts.email);
+            Employ employ6 = new Employ("Alexsandra", "something", new DateTime(2004, 7, 20), Country.Spain, Gender.female, Contacts.email);
+            Employ employ7 = new Employ("Marco", "something", new DateTime(2004, 7, 20), Country.Sweden, Gender.male, Contacts.email);
+            Employ employ8 = new Employ("Linda", "something", new DateTime(2004, 7, 20), Country.Sweden, Gender.female, Contacts.email);
+
+
+            Employ[] employs =
+            {
+                   employ1,
+                   employ2,
+                   employ3,
+                   employ4,
+                   employ5,
+                   employ6,
+                   employ7,
+                   employ8
+            };
+
+            Console.WriteLine("Employs from Georgia: ");
+            GetByCountry(employs, Country.Georgia);
+
+            Console.WriteLine("Employs from Spain: ");
+            GetByCountry(employs, Country.Spain);
+
+        }
+
+        public static void GetByCountry(Employ[] employs, Country country)
+        {
+            if (employs == null)
+                throw new ArgumentNullException(nameof(employs));
+
+            for(int i = 0; i < employs.Length; i++)
+            {
+                if (employs[i].Country == country)
+                {
+                    Console.WriteLine(employs[i].ToString());
+                }
+            }
+        }
+    }
+
+    
+}
