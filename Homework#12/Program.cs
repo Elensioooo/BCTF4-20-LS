06ceef7f376ecf76501fa025b9c13c259be5a173
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2026-06-22 17:28:12 +0700

    Add some methods to the Student class

--- Homework#12/Program.cs
+++ Homework#12/Program.cs
@@ -4,21 +4,88 @@
     {
         static void Main(string[] args)
         {
+            
             Student[] studentsArray =
             {
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
-                new Student("Elene", "Morgoshia", 21, "Email@gamil.com", "599555888", 4.0, Faculty.IT)
+                new Student("Sandro", "Bochorisvhili", 21, "Email@gamil.com", "599555888", 3.0, Faculty.Business),
+                new Student("Mariami", "Loria", 19, "Email@gamil.com", "599555888", 2.5, Faculty.Business),
+                new Student("Elene", "Morgoshia", 20, "Email@gamil.com", "599555888", 3.5, Faculty.IT),
+                new Student("Nikolozi", "Morgoshia", 17, "Email@gamil.com", "599555888", 2.0, Faculty.Design),
+                new Student("Alexandre", "Gvaramia", 17, "Email@gamil.com", "599555888", 3.8, Faculty.Business),
+                new Student("Demetre", "Vasadze", 18, "Email@gamil.com", "599555888", 4.0, Faculty.IT),
+                new Student("Gvantsa", "Vadachkoria", 21, "Email@gamil.com", "599555888", 2.4, Faculty.IT),
+                new Student("Lika", "Meladze", 21, "Email@gamil.com", "599555888", 2.1, Faculty.IT),
+                new Student("Ana", "Aslanisvhili", 22, "Email@gamil.com", "599555888", 3.9, Faculty.Business),
+                new Student("Giorgi", "beroshivili", 20, "Email@gamil.com", "599555888", 4.0, Faculty.Design),
             };
 
-            
+            foreach (Student student in studentsArray)
+            {
+                Console.WriteLine(student.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The best students are: ");
+            Student[] theBestStudents = Student.GetBestStudents(studentsArray);
+            foreach (Student student in theBestStudents)
+            {
+                Console.WriteLine(student.ToString());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("The best students again(with collection): ");
+            List<Student> bestStudents = Student.GetHighestGpaStudents(studentsArray);
+            foreach (Student student in bestStudents)
+            {
+                Console.WriteLine(student.ToString());
+            }
+
+            Console.WriteLine();
+            double gpaAverage = Student.GetGpaAverage(studentsArray);
+            Console.WriteLine($"Average of Students GPA: {gpaAverage}");
+
+            Console.WriteLine();
+            Console.WriteLine("Please Enter a lastName: ");
+            string studentLastName = Console.ReadLine();
+            if(Student.ContainsStudent(studentLastName, studentsArray))
+            {
+                List<Student> resultStudents = Student.GetStudentsByLastName(studentLastName, studentsArray);
+                Console.WriteLine($"Students whose lastname is {studentLastName}:");
+                foreach (Student student in resultStudents)
+                {
+                    Console.WriteLine(student.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no student with this lastName");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Please Enter a lastName: ");
+            string lastName = Console.ReadLine();
+            if(Student.ContainsStudent(lastName, studentsArray))
+            {
+                Student[] foundStudents = Student.FindStudentsByLastName(lastName, studentsArray);
+                Console.WriteLine($"Students whose lastname is {lastName}: ");
+                foreach (Student student in foundStudents)
+                {
+                    Console.WriteLine(student.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no student with this lastname");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Students sorted by GPA: ");
+            Student[] sortedByGpaStudents = Student.SortByGpa(studentsArray);
+            foreach(Student student in sortedByGpaStudents)
+            {
+                Console.WriteLine(student.ToString());
+            }
+            Console.ReadKey();
         }
     }
 }
--- Homework#12/Student.cs
+++ Homework#12/Student.cs
@@ -1,9 +1,10 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 
 namespace Homework_12
 {
-    internal class Student
+    internal class Student : IComparable<Student>
     {
         private string _name;
         private string _lastName;
@@ -85,7 +86,7 @@ namespace Homework_12
             {
                 if (string.IsNullOrWhiteSpace(value) || value.Length != 9)
                     throw new ArgumentException("Phone number must include 9 digits");
-                foreach(char c in value)
+                foreach (char c in value)
                 {
                     if (!char.IsDigit(c))
                         throw new ArgumentException("Phone number must inlude only digits");
@@ -119,6 +120,182 @@ namespace Homework_12
             {
                 _faculty = value;
             }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, LastName: {LastName}, Age: {Age}, Email: {Email}, Phone: {Phone}, GPA: {GPA}, Faculty: {Faculty}";
+        }
+
+        
+        public static Student[] GetBestStudents(Student[] studentsArray)
+        {
+            if (studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given array is empty");
+
+            int count = 0;
+            double highestGPA = studentsArray[0].GPA;
+            foreach(Student student in studentsArray)
+            {
+                if(student.GPA > highestGPA)
+                {
+                    highestGPA = student.GPA;
+                }
+            }
+
+            foreach(Student student in studentsArray)
+            {
+                if(highestGPA == student.GPA)
+                {
+                    count++;
+                }
+            }
+
+            Student[] bestStudents = new Student[count];
+            int index =0;
+            foreach(Student student in studentsArray)
+            {
+                if(highestGPA == student.GPA)
+                {
+                    bestStudents[index] = student;
+                    index++;
+                }
+            }
+
+            return bestStudents;
+        }
+
+        //with collections
+        public static List<Student> GetHighestGpaStudents(Student[] studentsArray)
+        {
+            if (studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given array is empty");
+
+            double highestGPA = studentsArray[0].GPA;
+            foreach(Student student in studentsArray)
+            {
+                if (student.GPA > highestGPA)
+                    highestGPA = student.GPA;
+            }
+
+            List<Student> highestGPAStudents = new List<Student>();
+            foreach (Student student in studentsArray)
+            {
+                if (highestGPA == student.GPA)
+                    highestGPAStudents.Add(student);
+            }
+
+            return highestGPAStudents;
+        }
+
+        public static double GetGpaAverage(Student[] studentsArray)
+        {
+            if (studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given array is empty");
+
+            double sum = 0;
+            double gpaAevrage;
+            foreach(Student student in studentsArray)
+            {
+                sum += student.GPA;
+            }
+
+            gpaAevrage = sum / studentsArray.Length;
+            return gpaAevrage;
+        }
+
+        //with collections
+        public static List<Student> GetStudentsByLastName(string lastName, Student[] studentsArray)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException("Given Lastname is emtpy");
+            if(studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given Array is empty");
+
+            List<Student> resultStudents = new List<Student>();
+            foreach(Student student in studentsArray)
+            {
+                if (student.LastName.ToLower().Contains(lastName.Trim().ToLower()))
+                    resultStudents.Add(student);
+            }
+
+            return resultStudents;
+        }
+
+        public static Student[] FindStudentsByLastName(string lastName, Student[] studentsArray)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException("Given Lastname is emtpy");
+            if (studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given Array is empty");
+
+            int count = 0;
+            foreach(Student student in studentsArray)
+            {
+                if (student.LastName.ToLower().Contains(lastName.Trim().ToLower()))
+                    count++;
+            }
+
+            Student[] foundStudents = new Student[count];
+            int index = 0;
+            foreach(Student student in studentsArray)
+            {
+                if (student.LastName.ToLower().Contains(lastName.Trim().ToLower()))
+                {
+                    foundStudents[index] = student;
+                    index++;
+                }
+                    
+            }
+            return foundStudents;
+        }
+
+        public static bool ContainsStudent(string lastName, Student[] studentsArray)
+        {
+            if (string.IsNullOrWhiteSpace(lastName))
+                throw new ArgumentException("Given Lastname is emtpy");
+            if (studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given Array is empty");
+
+            foreach(Student student in studentsArray)
+            {
+                if (student.LastName.ToLower().Contains(lastName.Trim().ToLower()))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+        public static Student[] SortByGpa(Student[] studentsArray)
+        {
+            if (studentsArray == null)
+                throw new ArgumentNullException(nameof(studentsArray));
+            if (studentsArray.Length == 0)
+                throw new ArgumentException("Given Array is empty");
+
+            Array.Sort(studentsArray);
+            Array.Reverse(studentsArray);
+            return studentsArray;
+        }
+
+        public int CompareTo(Student student)
+        {
+            if (student == null)
+                throw new ArgumentNullException(nameof(student));
+            return GPA.CompareTo(student.GPA);
         }
     }
 
